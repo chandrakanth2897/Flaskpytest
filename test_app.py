97818f36d53c1c7a4b8a5196fa5bec5c6e227ac9
chandrakanth2897 <121753583+chandrakanth2897@users.noreply.github.com>
2023-05-08 07:42:43 +0530

    Delete test_app.py

--- test_app.py
+++
@@ -1,7 +0,0 @@
-import app
-
-def test_index():
-    client = app.app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b'Hello, World!' in response.data
